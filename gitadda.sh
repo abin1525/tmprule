518c3b7dc1de618297255c2f44788e81b15319b3
abin <abin1525@163.com>
2017-12-04 05:57:17 -0800

    final init

---
+++ gitadda.sh
@@ -0,0 +1,3 @@
+cp '/home/demo/RULE_SELECTION' '/home/demo/rose-develop/projects/compass/extensions/checkers'
+cp '/home/demo/rose-develop/projects/compass/tools/compass/CHECKER_LIST' '/home/demo/rose-develop/projects/compass/extensions/checkers'
+git add -A
